6c683b6874720a8c061ab78fb55936ae658df39e
DineshReddyP <praneeth.t96@gmail.com>
2021-07-25 15:52:19 +0530

    created modules

---
+++ app/src/main/java/com/example/saveoassignment/views/MoviesHomePageActivity.kt
@@ -0,0 +1,12 @@
+package com.example.saveoassignment.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.saveoassignment.R
+
+class MoviesHomePageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movies_home_page)
+    }
+}
